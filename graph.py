8ba10258f0e85b98d2f87931118475b56c8cb895
MatthewCharleston <mcharleston@berkeley.edu>
2016-05-02 10:09:28 -0700

    post submission

--- graph.py
+++ graph.py
@@ -136,7 +136,7 @@ class Graph:
 class GraphUnitTests(unittest.TestCase):
     def test_DiffCycles(self):
         a = [[0 for x in range(7)] for y in range(7)]
-        a[0] = [0,0,0,0,0,0,1]
+        a[0] = [0,1,0,0,0,0,1]
         a[1] = [1,0,0,1,0,0,0]
         a[2] = [0,0,0,1,0,0,0]
         a[3] = [0,1,0,0,1,0,0]
@@ -208,4 +208,4 @@ def main():
 
 
 if __name__ == "__main__":
-    unittest.main()
+    main()
